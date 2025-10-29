2c58953990951d8a6ceed083cdf8cd10d885c75a
Sneh <snehptl28@gmail.com>
2025-10-29 13:03:08 +0530

    Ignore .env and node_modules; add .env.example

--- node_modules/strapi-plugin-redirects/admin/components/RedirectForm/types.js
+++
@@ -1,9 +0,0 @@
-const redirectTypeOptions = [
-  "found_302",
-  "moved_permanently_301",
-  "temporary_redirect_307",
-  "gone_410",
-  "unavailable_for_legal_reasons_451"
-];
-
-export { redirectTypeOptions };
--- node_modules/strapi-plugin-redirects/server/controllers/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const contentType = require('./content-type');
-const redirects = require('./redirects');
-
-module.exports = {
-  'content-type': contentType,
-  'redirects': redirects,
-};
